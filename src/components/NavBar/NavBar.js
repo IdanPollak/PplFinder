2848cb98f2de4f28329c875017616389ede83f6b
IdanPollak <58901496+IdanPollak@users.noreply.github.com>
2021-07-15 23:32:14 +0300

    Added functionality that enables user to add/remove users to/from the favorites list in the new Favorites page. (#2)
    1. Added functionality which enables the user to select their favorite users, refreshing or closing the site will not reset the favorites.
    2. Once a user is Favorited, the heart icon stays visible even if the user is not currently being hovered over.
    3. Clicking on a favorite user's heart icon in the home page list unfavorite the user.
    
    4. Favorites Page:
    4.1. The Navbar has a Favorites tab, which navigates to the new Favorite Page once clicked.
    4.2. The Favorite Page displays all the Favorited users.
    4.3. Clicking on a heart icon inside the favorite page unfavorite the user, and remove it from the page.

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -2,11 +2,13 @@ import React, { useState } from "react";
 import AppBar from "@material-ui/core/AppBar";
 import Tabs from "@material-ui/core/Tabs";
 import Tab from "@material-ui/core/Tab";
+import { Link } from "react-router-dom";
 
 const NavBar = () => {
-  const [value, setValue] = useState(0);
+  const [value, setValue] = useState(sessionStorage.getItem('tabValue') === null? 0 : JSON.parse(sessionStorage.getItem('tabValue')));
 
   const handleChange = (_e, newValue) => {
+    sessionStorage.setItem('tabValue', newValue);
     setValue(newValue);
   };
 
@@ -19,8 +21,8 @@ const NavBar = () => {
         indicatorColor="primary"
         textColor="primary"
       >
-        <Tab label="Home" index={0} />
-        <Tab label="Favorites" index={1} />
+        <Tab label="Home" to="/" component={Link} />
+        <Tab label="Favorites" to="/Favorites" component={Link} />
       </Tabs>
     </AppBar>
   );
--- src/components/UserList/UserList.js
+++ src/components/UserList/UserList.js
@@ -1,15 +1,29 @@
-import React, { useEffect, useState } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import Text from "components/Text";
 import Spinner from "components/Spinner";
 import CheckBox from "components/CheckBox";
 import IconButton from "@material-ui/core/IconButton";
 import FavoriteIcon from "@material-ui/icons/Favorite";
+import Alert from '@material-ui/lab/Alert';
+import Snackbar from '@material-ui/core/Snackbar';
+import Context from 'store/Context';
 import * as S from "./style";
 
 const UserList = ({ users, isLoading }) => {
 
   const [hoveredUserId, setHoveredUserId] = useState();
   const [countryFiltersList, setCountryFiltersList] = useState([]);
+  const [errorMsg, setErrorMsg] = useState(false);
+  const [successMsg, setSuccessMsg] = useState(false);
+
+  const context = useContext(Context);
+
+  const handleClose = (event, reason) => {
+    if (reason === 'clickaway') {
+      return;
+    }
+    (successMsg) ? setSuccessMsg(false) : setErrorMsg(false);    
+  };
 
   const filteredList = () => {
     return (countryFiltersList.length > 0)? users.filter(user => countryFiltersList.includes(user.location.country)) : users;
@@ -19,6 +33,21 @@ const UserList = ({ users, isLoading }) => {
     (countryFiltersList.includes(countryFilter))?
      setCountryFiltersList(countryFiltersList.filter(country => country !== countryFilter)):
       setCountryFiltersList(currentCountryFiltersList => [...currentCountryFiltersList, countryFilter]);
+  };
+
+  const onUserClick = (clickedUser) => {
+    let newFavoritesList = [];
+    if(context.favoritesList.includes(clickedUser)) {
+      newFavoritesList = context.favoritesList.filter(favoriteUser => favoriteUser !== clickedUser);
+      setErrorMsg(true);
+    }
+    else {
+      newFavoritesList = [...context.favoritesList, clickedUser];
+      setSuccessMsg(true);
+    }
+
+    context.setFavoritesList(newFavoritesList);
+    localStorage.setItem('favorites', JSON.stringify(newFavoritesList));
   };
 
   const handleMouseEnter = (index) => {
@@ -59,8 +88,8 @@ const UserList = ({ users, isLoading }) => {
                   {user?.location.city} {user?.location.country}
                 </Text>
               </S.UserInfo>
-              <S.IconButtonWrapper isVisible={index === hoveredUserId}>
-                <IconButton>
+              <S.IconButtonWrapper isVisible={context.favoritesList.includes(user) || index === hoveredUserId}>
+                <IconButton onClick={() => onUserClick(user)}>
                   <FavoriteIcon color="error" />
                 </IconButton>
               </S.IconButtonWrapper>
@@ -73,6 +102,16 @@ const UserList = ({ users, isLoading }) => {
           </S.SpinnerWrapper>
         )}
       </S.List>
+      <Snackbar open={successMsg} autoHideDuration={3000} onClose={handleClose}>
+      <Alert onClose={handleClose} severity="success" variant="filled">
+        User added to favorites!
+      </Alert> 
+      </Snackbar>
+      <Snackbar open={errorMsg} autoHideDuration={3000} onClose={handleClose}>
+      <Alert onClose={handleClose} severity="error" variant="filled">
+       User removed from favorites!
+      </Alert>
+      </Snackbar>
     </S.UserList>
   );
 };
