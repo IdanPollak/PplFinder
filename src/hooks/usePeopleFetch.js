fc92b57ad1be8b6326178f9c3e6fbe6509475bbb
IdanPollak <idanpol1@gmail.com>
2021-07-16 17:06:59 +0300

    Implemented an Infinity Scroll functionality using react-infinite-scroll-component, which fetch additional users and add them to the list once scrolling all the way down. Instead of fetching the maximum amount of users from randomUser API(5000) - fetching the next new page (25 users) every time that the user is at the bottom of the list to avoid critical performance issues.

--- src/hooks/usePeopleFetch.js
+++ src/hooks/usePeopleFetch.js
@@ -4,17 +4,18 @@ import axios from "axios";
 export const usePeopleFetch = () => {
   const [users, setUsers] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
+  const [page, setPage] = useState(1);
 
   useEffect(() => {
     fetchUsers();
-  }, []);
+  }, [page]);
 
   async function fetchUsers() {
     setIsLoading(true);
-    const response = await axios.get(`https://randomuser.me/api/?results=25&page=1`);
+    const response = await axios.get(`https://randomuser.me/api/?results=25&page=${page}&seed=abc`);
     setIsLoading(false);
-    setUsers(response.data.results);
+    setUsers(users.concat(Array.from(response.data.results)));    
   }
 
-  return { users, isLoading, fetchUsers };
+  return { users, isLoading, page, setPage, fetchUsers };
 };
